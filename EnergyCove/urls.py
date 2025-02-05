b72cba793b6e768c371d310805c5f53e86296eeb
abahmad2 <abdullah.ahmad@sabanciuniv.edu>
2025-02-05 19:09:18 +0300

    Fixed urls.py

--- EnergyCove/urls.py
+++ EnergyCove/urls.py
@@ -1,6 +1,8 @@
 # EnergyCove/urls.py
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('solar.urls')),
 ]
